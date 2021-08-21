2125b1fa2efd7d0d890522be0eb8c40032df1b50
Mason Adrales <masonadrales@Masons-MacBook-Pro-4.local>
2021-08-20 19:51:06 -0500

    Added initial styling and two components

---
+++ masonadralescom/src/components/Navbar/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Wrapper, Content} from './Navbar.styles';
+
+const Navbar = () => (
+    <Wrapper>
+        <Content>
+            Hello World!
+        </Content>
+    </Wrapper>
+);
+
+export default Navbar;
